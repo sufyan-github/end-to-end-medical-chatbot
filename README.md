71725d83bae0a88f9c6a27bbd5d725040e12153c
sufyan-github <abusufyan.cse20@gmail.com>
2025-09-03 03:21:19 +0600

    Folder Structure added

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # end-to-end-medical-chatbot
+
+# How to run?
+### Steps
+
+Clone the repository
+
+```bash
+Project repo: https://github.com/sufyan-github/end-to-end-medical-chatbot.git
+
+```bash
+conda create -n medibot python
---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import find_packages,setup
+
+setup(
+    name="Generative AI Project",
+    version="0.0.0",
+    author="Md Abu Sufyan",
+    author_email="abusufyan.cse20@gmail.com",
+    packages=find_packages(),
+    install_requires=[
+        
+    ]
+)
